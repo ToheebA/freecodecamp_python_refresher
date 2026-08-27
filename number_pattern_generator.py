221d9a170e685fa7b52e02e948063150eb5f9e70
Toheeb Olufade <olufade.toheeb@gmail.com>
2026-08-27 16:14:27 +0100

    create number pattern generator app

---
+++ number_pattern_generator.py
@@ -0,0 +1,12 @@
+def number_pattern(n):
+    if not isinstance(n, int):
+        return "Argument must be an integer value."
+    elif n < 1:
+        return "Argument must be an integer greater than 0."
+    else:
+        pattern = ""
+        for i in range(1, n+1):
+            pattern += str(i) + " "
+        return pattern.strip()
+
+print(number_pattern(4))
